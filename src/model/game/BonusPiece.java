3db01b86261731093cafbd0a7593b0bf827b0582
Leduc Axel <leduc.axel.pro@gmail.com>
2018-04-20 21:25:11 +0200

    BonusPiece

--- src/model/game/BonusPiece.java
+++ src/model/game/BonusPiece.java
@@ -1,6 +1,11 @@
 package model.game;
 
 public class BonusPiece extends FieldPiece {
+
+    public BonusPiece(int x, int y, Ship parent) {
+        super(x, y, parent);
+    }
+
     @Override
     public Ship getParent() {
         return null;
